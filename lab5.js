6a386891193b1ff9aab3346cc7c0e02f203a6cd5
 Tahasyedddd <ehtxsham7@gmail.com>
2023-11-11 12:15:36 -0500

    add x-red

--- lab5.js
+++ lab5.js
@@ -5,3 +5,11 @@ class CenterHtml extends HTMLElement{
 }
 
 customElements.define("x-center",CenterHtml);
+
+class Redtml extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="color:red">${this.innerHTML}</div>`;
+    }
+}
+
+customElements.define("x-red",red.html);
